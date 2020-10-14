34d02f1ac0b9410d7081c9b04c4a5dd1b3a64c69
ihranova <hranova.developer@gmail.com>
2020-10-14 11:40:48 +0300

    fix

--- searchGoogle.js
+++ searchGoogle.js
@@ -1,59 +1,60 @@
 const puppeteer = require('puppeteer');
+const {performance} = require('perf_hooks');
 
 // This is where we'll put the code to get around the tests.
 const preparePageForTests = async (page) => {
-    // Pass the User-Agent Test.
-    const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
-      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
-    await page.setUserAgent(userAgent);
-  
-    // Pass the Webdriver Test.
-    await page.evaluateOnNewDocument(() => {
-      Object.defineProperty(navigator, 'webdriver', {
-        get: () => false,
-      });
-    });
-  
-    // Pass the Chrome Test.
-    await page.evaluateOnNewDocument(() => {
-      // We can mock this in as much depth as we need for the test.
-      window.navigator.chrome = {
-        runtime: {},
-        // etc.
-      };
-    });
-  
-    // Pass the Permissions Test.
-    await page.evaluateOnNewDocument(() => {
-      const originalQuery = window.navigator.permissions.query;
-      return window.navigator.permissions.query = (parameters) => (
-        parameters.name === 'notifications' ?
-          Promise.resolve({ state: Notification.permission }) :
-          originalQuery(parameters)
-      );
-    });
-  
-    // Pass the Plugins Length Test.
-    await page.evaluateOnNewDocument(() => {
-      // Overwrite the `plugins` property to use a custom getter.
-      Object.defineProperty(navigator, 'plugins', {
-        // This just needs to have `length > 0` for the current test,
-        // but we could mock the plugins too if necessary.
-        get: () => [1, 2, 3, 4, 5],
-      });
-    });
-  
-    // Pass the Languages Test.
-    await page.evaluateOnNewDocument(() => {
-      // Overwrite the `plugins` property to use a custom getter.
-      Object.defineProperty(navigator, 'languages', {
-        get: () => ['en-US', 'en'],
-      });
-    });
-  }
-  
+  // Pass the User-Agent Test.
+  const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
+    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+  await page.setUserAgent(userAgent);
 
-/*const searchGoogle = async (searchQuery, url) => {
+  // Pass the Webdriver Test.
+  await page.evaluateOnNewDocument(() => {
+    Object.defineProperty(navigator, 'webdriver', {
+      get: () => false,
+    });
+  });
+
+  // Pass the Chrome Test.
+  await page.evaluateOnNewDocument(() => {
+    // We can mock this in as much depth as we need for the test.
+    window.navigator.chrome = {
+      runtime: {},
+      // etc.
+    };
+  });
+
+  // Pass the Permissions Test.
+  await page.evaluateOnNewDocument(() => {
+    const originalQuery = window.navigator.permissions.query;
+    return window.navigator.permissions.query = (parameters) => (
+      parameters.name === 'notifications' ?
+        Promise.resolve({ state: Notification.permission }) :
+        originalQuery(parameters)
+    );
+  });
+
+  // Pass the Plugins Length Test.
+  await page.evaluateOnNewDocument(() => {
+    // Overwrite the `plugins` property to use a custom getter.
+    Object.defineProperty(navigator, 'plugins', {
+      // This just needs to have `length > 0` for the current test,
+      // but we could mock the plugins too if necessary.
+      get: () => [1, 2, 3, 4, 5],
+    });
+  });
+
+  // Pass the Languages Test.
+  await page.evaluateOnNewDocument(() => {
+    // Overwrite the `plugins` property to use a custom getter.
+    Object.defineProperty(navigator, 'languages', {
+      get: () => ['en-US', 'en'],
+    });
+  });
+}
+
+
+const searchGoogle = async (searchQuery, url) => {
     //const browser = await puppeteer.launch();
     const args = [
         '--no-sandbox',
@@ -67,6 +68,7 @@ const preparePageForTests = async (page) => {
         
         
     const browser = await puppeteer.launch({args, headless: false}); // default is true
+    //await preparePageForTests(page);
 
     const page = await browser.newPage();
 
@@ -175,29 +177,9 @@ const preparePageForTests = async (page) => {
     await browser.close();
     //console.log(searchResults);
     return searchResults;
-};*/
-(async () => {
-    // Launch the browser in headless mode and set up a page.
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox'],
-      headless: true,
-    });
-    const page = await browser.newPage();
-  
-    // Prepare for the tests (not yet implemented).
-    await preparePageForTests(page);
-  
-    // Navigate to the page that will perform the tests.
-    const testUrl = 'https://google.com';
-    await page.goto(testUrl);
-  
-    // Save a screenshot of the results.
-    await page.screenshot({path: 'example.png'});
-  
-    // Clean up.
-    await browser.close()
-  })();
+};
 
-//module.exports = searchGoogle;
+
+module.exports = searchGoogle;
 
 //searchGoogle('cats');
